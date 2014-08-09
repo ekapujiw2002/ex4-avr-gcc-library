a62ee41dd845d8569a99fbe67970ebc0b1927ce1
ekapujiw2002 <ekapujiw2002@gmail.com>
2014-08-09 13:20:52 +0700

    Sampel lcd with sipo chip 74hc595

--- lcd_sipo_tes.c
+++ lcd_sipo_tes.c
@@ -8,34 +8,45 @@
 #include <avr/pgmspace.h>
 #include <util/delay.h>
 #include "lib/lcd_aligned/lcd_lib.h"
-#include <stdlib.h>
-#include <string.h>
 
 int main(void)
 {
 	lcd_t lcd1 = lcd_struct_default;
 	uint16_t xx=0;
-	uint8_t strx[16];
 	
 	lcd1.initialize();
 	lcd1.clear();
 	lcd1.cursor_off();
 	lcd1.home();
-	lcd1.flash_text((const uint8_t*)PSTR("HELLO WORLD"),0,0);
-	//LCDsendChar('A');
+	lcd1.flash_text((const uint8_t*)PSTR("HELLO WORLD"),0,0);	
+	lcd1.flash_text((const uint8_t*)PSTR("DEMO LCD SIPO"),0,1);
+	//lcd1.flash_text((const uint8_t*)PSTR("74HC595"),0,2);
+	//lcd1.flash_text((const uint8_t*)PSTR("BY EX4"),0,3);
+	_delay_ms(2000);
+	
+	//LCDhome();
+	for (xx = 0; xx < 10; xx++)
+	{
+		lcd1.shift_left(1);
+		_delay_ms(500);
+	}
+	
+	for (xx = 0; xx < 16; xx++)
+	{
+		lcd1.shift_right(1);
+		_delay_ms(500);
+	}
+	
+	lcd1.clear();
 	
 	for (;;)
 	{
-		//utoa(xx,(char*)strx,10);
-		//lcd1.position(0,1);
-		//lcd1.text(strx,strlen((char*)strx));
-		lcd1.printf(LCD_USE_FLASH,0,1,PSTR("%-5d"),xx);
-		lcd1.progress_bar(0,2,LCD_PROGRESSBAR_FORWARD,(uint8_t)xx,255,20);
+		lcd1.printf(LCD_USE_FLASH,0,0,PSTR("%-5d"),xx);
+		lcd1.progress_bar(0,1,LCD_PROGRESSBAR_FORWARD,(uint8_t)xx,255,16);
 		xx++;
 		if (xx>1000)
 		{
 			xx=0;
 		}
-		//_delay_ms(100);
 	}
 }
