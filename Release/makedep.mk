1d6804e107a2fa265c4bac491b03cbfbaf4d7b28
ekapujiw2002 <ekapujiw2002@gmail.com>
2014-08-18 06:20:44 +0700

    Updated makefile

--- Release/makedep.mk
+++ Release/makedep.mk
@@ -2,9 +2,13 @@
 # Automatically-generated file. Do not edit or delete the file
 ################################################################################
 
-lcdmixed.c
+usart-lib-tes.c
 
 lib\adc\adc.c
 
-lib\lcd_aligned\lcd_lib.c
+lib\i2c_master\I2C_master.c
+
+lib\lcd_mixed\lcd_mixed.c
+
+lib\usart_ex4\usart_lib.c
 
